76441cb14a04cad611392463210a0eac42bbabf6
David Thorpe <djt@mutablelogic.com>
2016-05-04 11:18:03 +0200

    added version information

---
+++ build/version.go
@@ -0,0 +1,11 @@
+/*
+  Copyright David Thorpe 2015 All Rights Reserved
+  Please see file LICENSE for information on distribution, etc
+*/
+package ytapi
+
+const (
+  VERSION_COPYRIGHT = "Copyright David Thorpe 2015 All Rights Reserved"
+  VERSION_AUTHOR = "David Thorpe <djt@mutablelogic.com>"
+  VERSION_TAG = "v0.1"
+)
