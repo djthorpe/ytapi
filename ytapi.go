deff9e04af9665f25fe7a8ddcec83448a8317823
David Thorpe <davidthorpe@google.com>
2016-05-11 14:21:25 +0100

    Cleaned up the Authenticate method

--- ytapi.go
+++ ytapi.go
@@ -120,51 +120,6 @@ func main() {
 }
 
 /*
-func main2() {
-	// Parse command-line flags
-	command, values, err := ytapi.ParseFlags([]*ytapi.RegisterFunction{
-		&ytapi.RegisterFunction{ Callback: ytcommands.RegisterAuthenticateCommands, Title: "Authentication opetations" },
-		&ytapi.RegisterFunction{ Callback: ytcommands.RegisterChannelCommands, Title: "Operations on Channels" },
-		&ytapi.RegisterFunction{ Callback: ytcommands.RegisterChannelSectionCommands, Title: "Channel Section operations" },
-		&ytapi.RegisterFunction{ Callback: ytcommands.RegisterVideoCommands, Title: "Operations on videos" },
-		&ytapi.RegisterFunction{ Callback: ytcommands.RegisterBroadcastCommands, Title: "Operations on Broadcasts" },
-		&ytapi.RegisterFunction{ Callback: ytcommands.RegisterStreamCommands, Title: "Operations on Streams" },
-		&ytapi.RegisterFunction{ Callback: ytcommands.RegisterCaptionCommands, Title: "Operations on video captions" },
-		&ytapi.RegisterFunction{ Callback: ytcommands.RegisterPlaylistCommands, Title: "Operations on Playlists" },
-		&ytapi.RegisterFunction{ Callback: ytcommands.RegisterPlaylistItemCommands, Title: "Operations on PlaylistItems" },
-		&ytapi.RegisterFunction{ Callback: ytcommands.RegisterLanguageRegionCommands, Title: "Language and Region operations" },
-		&ytapi.RegisterFunction{ Callback: ytcommands.RegisterSearchCommands, Title: "Search operations" },
-		&ytapi.RegisterFunction{ Callback: cidcommands.RegisterContentOwnerCommands, Title: "Content owner operations" },
-		&ytapi.RegisterFunction{ Callback: cidcommands.RegisterPolicyCommands, Title: "Policy operations" },
-		&ytapi.RegisterFunction{ Callback: cidcommands.RegisterClaimCommands, Title: "Claim operations" },
-        &ytapi.RegisterFunction{ Callback: cidcommands.RegisterAssetCommands, Title: "Asset operations" },
-        &ytapi.RegisterFunction{ Callback: cidcommands.RegisterReferenceCommands, Title: "Reference operations" },
-	})
-	if err != nil {
-		// Error occured in command setup
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		os.Exit(1)
-	}
-	if command == nil {
-		// No command is to be executed, so exit
-		os.Exit(0)
-	}
-
-	// Read content owner and channel from file
-	defaultsPath := ytcommands.GetDefaultsPath(values)
-	if err := values.ReadDefaultsFromFile(defaultsPath); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		os.Exit(1)
-	}
-
-	// call command setup function
-	output := ytapi.NewTable()
-	if command.Setup != nil {
-		if err := command.Setup(values, output); err != nil {
-			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-			os.Exit(1)
-		}
-	}
 
 	// add and remove fields from the table
 	if values.IsSet(&ytapi.FlagFields) {
@@ -185,38 +140,6 @@ func main2() {
 		}
 	}
 
-	// create the service object
-	serviceAccountPath := ytcommands.GetServiceAccountPath(values)
-	clientSecretPath := ytcommands.GetClientSecretPath(values)
-	tokenPath := ytcommands.GetOAuthTokenPath(values)
-	debugFlag := values.GetBool(&ytapi.FlagDebug)
-
-	// if the content owner is set, then create an API object from service
-	// account, or else create the API object from client secrets and oauth
-	// token
-	var service *ytservice.Service
-	if values.IsSet(&ytapi.FlagContentOwner) {
-		service, err = ytservice.NewYouTubeServiceFromServiceAccountJSON(serviceAccountPath, debugFlag)
-	} else {
-		service, err = ytservice.NewYouTubeServiceFromClientSecretsJSON(clientSecretPath, tokenPath, debugFlag)
-	}
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		os.Exit(1)
-	}
-
-	// call command execute function
-	if command.Execute != nil {
-        // check for service account
-        if (command.ServiceAccount==true) && (service.ServiceAccount==false) {
-            fmt.Fprintf(os.Stderr, "Error: Requires a service account\n")
-            os.Exit(1)
-        }
-		if err := command.Execute(service, values, output); err != nil {
-			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-			os.Exit(1)
-		}
-	}
-}
 */
+
 
