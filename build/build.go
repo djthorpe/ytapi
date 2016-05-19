0fa2f8f45c1e3c40034be972348dbdacf0308991
David Thorpe <davidthorpe@google.com>
2016-05-19 12:32:50 +0100

    Updated building

--- build/build.go
+++ build/build.go
@@ -5,51 +5,77 @@
 package main
 
 import (
-    "os"
-    "flag"
-    "fmt"
-    "encoding/json"
-    "text/template"
+	"encoding/json"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"os"
+	"text/template"
+	"io/ioutil"
 )
 
 type Settings struct {
-    Branch string
-    Tag string
-    Hash string
-    Date string
-    GoVersion string
+	Branch         string `json:"branch"`
+	Tag            string `json:"tag"`
+	Hash           string `json:"hash"`
+	Date           string `json:"date"`
+	GoVersion      string `json:"goversion"`
+	ClientSecret   string `json:"client_secret"`
+	ServiceAccount string `json:"service_account"`
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
+func decodefile(filename string) (string,error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return "",err
+	}
+	return base64.StdEncoding.EncodeToString(data),nil
+}
+
 func main() {
-    var settings Settings
+	var settings Settings
 
-    flag.Parse()
+	flag.Parse()
 
-    // Read in JSON file
-    args := flag.Args()
-    if len(args) != 2 {
-        fmt.Fprintln(os.Stderr,"Syntax error: requires input json and template arguments")
-        os.Exit(1)
-    }
-    file, err := os.Open(args[0])
-    if err != nil {
-        fmt.Fprintln(os.Stderr,err)
-        os.Exit(1)
-    }
-    defer file.Close()
-    json := json.NewDecoder(file)
-    if err = json.Decode(&settings); err != nil {
-        fmt.Fprintln(os.Stderr,err)
-        os.Exit(1)
-    }
+	// Read in JSON file
+	args := flag.Args()
+	if len(args) != 2 {
+		fmt.Fprintln(os.Stderr, "Syntax error: requires input json and template arguments")
+		os.Exit(1)
+	}
+	file, err := os.Open(args[0])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer file.Close()
+	json := json.NewDecoder(file)
+	if err = json.Decode(&settings); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 
-    // Use template to output
-    t,err := template.ParseFiles(args[1])
-    if err != nil {
-        fmt.Fprintln(os.Stderr,err)
-        os.Exit(1)
-    }
-    t.Execute(os.Stdout,settings)
+	// Convert Client Secret and Service Account
+	if settings.ClientSecret != "" {
+		if settings.ClientSecret,err = decodefile(settings.ClientSecret); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
+	if settings.ServiceAccount != "" {
+		if settings.ServiceAccount,err = decodefile(settings.ServiceAccount); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
+
+	// Use template to output
+	t, err := template.ParseFiles(args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	t.Execute(os.Stdout, settings)
 }
--- build/build.sh
+++ build/build.sh
@@ -1,39 +1,94 @@
 #!/bin/bash
 # Shell script to determine what the current build of the
-# repository is
+# repository is, and encode it into a go file - then compile
+# the ytapi binary. To use the command, you can run the
+# shell without any flags, but you can also wrap a client
+# secrets and service account file into the binary so that
+# it's easy to install these by running the ytapi binary
+# without having to do lots of copying afterwards. So:
+#
+# build.sh
+# build.sh -c <client_secret.json>
+# build.sh -c <client_secret.json> -s <service_account.json>
+#
+# are all valid ways of compiling a binary of the software
 
 ##############################################################
 
 CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-JSON_PATH="${CURRENT_PATH}/version.json"
+JSON_PATH="${TMPDIR}/version.json"
 TEMPLATE_PATH="${CURRENT_PATH}/version.go.tmpl"
 VERSION_PATH="${CURRENT_PATH}/../version.go"
 GIT=`which git`
 GO=`which go`
 
 ##############################################################
-# Sanity checks
 
 cd "${CURRENT_PATH}/.."
+
+##############################################################
+# Sanity checks
+
+if [ ! -d ${TMPDIR} ] ; then
+  echo "Temporary directory $TMPDIR not found" >&2
+  exit -1
+fi
 if [ ! -d ${CURRENT_PATH} ] ; then
-    echo "Not found: ${CURRENT_PATH}"
+  echo "Not found: ${CURRENT_PATH}" >&2
+  exit -1
 fi
 if [ ! -f ${TEMPLATE_PATH} ] ; then
-  echo "Not found: ${TEMPLATE_PATH}"
+  echo "Template not found: ${TEMPLATE_PATH}" >&2
+  exit -1
 fi
 if [ ! -x ${GIT} ] ; then
-  echo "Not found: ${GIT}"
+  echo "git not installed or executable" >&2
+  exit -1
 fi
 if [ ! -x ${GO} ] ; then
-  echo "Not found: ${GO}"
+  echo "go not installed or executable" >&2
+  exit -1
 fi
 
+##############################################################
+# Check for client secret (-c) and/or service account flag (-s)
+# which include the client secret info and service account
+# info when generating the version.json file
+
+while getopts ':c:s:' FLAG ; do
+  case ${FLAG} in
+    c)
+	  CLIENT_SECRET=${OPTARG}
+      ;;
+    s)
+	  SERVICE_ACCOUNT=${OPTARG}
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG"
+	  exit -1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument"
+      exit -1
+      ;;
+  esac
+done
+
+if [ "${CLIENT_SECRET}" != "" ] && [ ! -f ${CLIENT_SECRET} ] ; then
+  echo "Not found: ${CLIENT_SECRET}" >&2
+  exit -1
+fi
+
+if [ "${SERVICE_ACCOUNT}" != "" ] && [ ! -f ${SERVICE_ACCOUNT} ] ; then
+  echo "Not found: ${SERVICE_ACCOUNT}" >&2
+  exit -1
+fi
 
 ##############################################################
 # Determine version
 
 # get our version info from git
-TAG=`${GIT} describe --tags`
+TAG=`${GIT} describe --tags 2>/dev/null`
 BRANCH=`${GIT} name-rev HEAD --name-only --always`
 HASH=`${GIT} rev-parse HEAD`
 DATE=`date -u`
@@ -45,12 +100,15 @@ echo "  \"branch\":\"${BRANCH}\"," >> ${JSON_PATH}
 echo "  \"tag\":\"${TAG}\"," >> ${JSON_PATH}
 echo "  \"hash\":\"${HASH}\"," >> ${JSON_PATH}
 echo "  \"date\":\"${DATE}\"," >> ${JSON_PATH}
-echo "  \"goversion\":\"${GOVERSION}\"" >> ${JSON_PATH}
+echo "  \"goversion\":\"${GOVERSION}\"," >> ${JSON_PATH}
+echo "  \"client_secret\":\"${CLIENT_SECRET}\"," >> ${JSON_PATH}
+echo "  \"service_account\":\"${SERVICE_ACCOUNT}\"" >> ${JSON_PATH}
 echo "}" >> ${JSON_PATH}
 
 # build the command line tool
 ${GO} run build/build.go ${JSON_PATH} ${TEMPLATE_PATH} > ${VERSION_PATH}
 ${GO} install ytapi.go version.go
 
-ls -l ${GOBIN}/ytapi
+# output
+cat ${JSON_PATH}
 
